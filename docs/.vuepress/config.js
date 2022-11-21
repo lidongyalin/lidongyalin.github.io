5425bf05badc65f6023585782de80f4981995687
李栋 <lidongyalin@gmail.com>
2022-11-21 13:08:52 +0800

    fix: 文字修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const themeConfig = require('./config/theme/')
 
 module.exports = {
-  title: "午后南杂",
+  title: "白梦泽",
   description: 'Enjoy when you can, and endure when you must.',
   dest: 'docs/.vuepress/dist',
   base:'/',
