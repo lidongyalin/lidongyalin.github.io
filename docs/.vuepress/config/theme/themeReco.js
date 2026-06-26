3674f6ddf47c5ea2f1bea8aa99e0198353e7ddc5
lidongyalin <lidongyalin@qq.com>
2026-06-26 09:15:19 +0000

    导航栏菜单改为中文

--- docs/.vuepress/config/theme/themeReco.js
+++ docs/.vuepress/config/theme/themeReco.js
@@ -4,15 +4,15 @@ module.exports = {
   blogConfig: {
     category: {
       location: 2, // 在导航栏菜单中所占的位置，默认2
-      text: 'Category' // 默认 “分类”
+      text: '分类'
     },
     tag: {
       location: 3, // 在导航栏菜单中所占的位置，默认3
-      text: 'Tag' // 默认 “标签”
+      text: '标签'
     }
   },
   // 最后更新时间
-  lastUpdated: 'Last Updated', // string | boolean
+  lastUpdated: '最后更新', // string | boolean
   // 作者
   author: '霒蚀君',
   // 备案号
